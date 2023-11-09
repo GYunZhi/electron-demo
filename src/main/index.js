6969eace46b3fe8675df6e891d7a24605d6111d4
gongyunzhi <gongyunzhi@zuoyebang.com>
2023-11-09 12:35:58 +0800

    chore: 调试

--- src/main/index.js
+++ src/main/index.js
@@ -1,20 +1,30 @@
-import { app, BrowserWindow, Menu, ipcMain } from 'electron'
-import * as apis from 'electron'
-import CookieManager from './utils/cookie'
+import { app, BrowserWindow, Menu, ipcMain, Notification } from 'electron';
+import * as apis from 'electron';
+import CookieManager from './utils/cookie';
 
 /**
  * Set `__static` path to static files in production
  * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
  */
 if (process.env.NODE_ENV !== 'development') {
-  global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
+  global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\');
 }
 
 // 单一实例
-const gotTheLock = app.requestSingleInstanceLock()
+const gotTheLock = app.requestSingleInstanceLock();
 
-let mainWindow
-const winURL = process.env.NODE_ENV === 'development' ? `http://localhost:9080` : `file://${__dirname}/index.html`
+let mainWindow;
+const winURL = process.env.NODE_ENV === 'development' ? `http://localhost:9080` : `file://${__dirname}/index.html`;
+
+// 创建通知函数
+function showNotification() {
+  const notification = new Notification({
+    title: '消息通知',
+    body: '你有一条新消息未查看',
+  });
+
+  notification.show();
+}
 
 /**
  * 创建主窗口
@@ -36,66 +46,74 @@ function createWindow() {
       preload: require('path').resolve(__dirname, '../preload/index.js'), // 在 preload 脚本中访问 node 的 API
 
       webSecurity: false, // 关闭启同源策略，默认是开启的
-      devTools: true
-    }
-  })
+      devTools: true,
+    },
+  });
 
-  mainWindow.loadURL(winURL)
+  mainWindow.loadURL(winURL);
   // mainWindow.loadFile(require('path').resolve(__dirname, '../renderer/test.html'))
 
   mainWindow.on('closed', () => {
-    mainWindow = null
-  })
+    mainWindow = null;
+  });
 }
 
-Menu.setApplicationMenu(null)
+Menu.setApplicationMenu(null);
 
 /**
  * 单一实例
  */
 if (!gotTheLock) {
-  app.quit()
+  app.quit();
 } else {
   app.on('second-instance', (event, commandLine, workingDirectory) => {
     // 当运行第二个实例时，将会聚焦到 mainWindow 这个窗口
     if (mainWindow) {
       if (mainWindow.isMinimized()) {
-        mainWindow.restore()
+        mainWindow.restore();
       }
-      mainWindow.focus()
+      mainWindow.focus();
     }
-  })
+  });
 
   app.on('ready', () => {
-    createWindow()
+    createWindow();
 
     /************************** 实例化工具类 ****************************/
-    const cookieManager = new CookieManager()
-
+    const cookieManager = new CookieManager();
 
     /************************** 接收渲染进程的消息 ****************************/
     // 同步消息
-    ipcMain.on('sync-render', (event, data) => {
-      event.returnValue = '我收到了同步消息，这是我的回复：啦啦啦'
-    })
+    ipcMain.on('sync-render', (event, args) => {
+      event.returnValue = '我收到了同步消息，这是我的回复：啦啦啦';
+    });
 
     // 异步消息（send）
-    ipcMain.on('async-render', (event, data) => {
-      event.reply('async-reply', '我收到了异步消息，这是我的回复：啦啦啦')
-    })
+    ipcMain.on('async-render', (event, args) => {
+      event.reply('async-reply', '我收到了异步消息，这是我的回复：啦啦啦');
+    });
 
     // 异步消息（invoke）
     // 注意：相同的事件名称，on 方法可以注册多次，但是 handle 方法只能注册一次，否则会报错
     ipcMain.handle('invoke-render', (event, args) => {
-      return '我收到了异步消息(invoke)，这是我的回复：啦啦啦'
-    })
-    
+      return '我收到了异步消息(invoke)，这是我的回复：啦啦啦';
+    });
+
+    // 监听从渲染进程发送的 Notification 请求
+    ipcMain.on('show-notification', (event, args) => {
+      showNotification();
+    });
+
+    // 监听从渲染进程发送的更新 Dock 图标数字的请求
+    ipcMain.on('update-dock-badge', (event, badgeCount) => {
+      // 设置 Dock 图标的数字
+      app.dock.setBadge(badgeCount.toString());
+    });
 
     /************************** 向渲染进程发送消息 ****************************/
     ipcMain.handle('send-a-message', (event, args) => {
-      mainWindow.webContents.send('main-msg', `这是一条来自主进程的消息 + ${new Date().getTime()}`)
-    })
-
+      mainWindow.webContents.send('main-msg', `这是一条来自主进程的消息 + ${new Date().getTime()}`);
+    });
 
     /************************** 设置全局变量 ****************************/
     global.sharedData = {
@@ -106,32 +124,32 @@ if (!gotTheLock) {
       chrome: process.versions.chrome,
       electron: process.versions.electron,
 
-      cookieManager
-    }
-  })
+      cookieManager,
+    };
+  });
 }
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
+});
 
 app.on('activate', () => {
   // if (mainWindow === null) {
   //   createWindow()
   // }
   app.on('activate', function () {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
 
 // 网页应用调起桌面程序
 // This will catch clicks on links such as <a href="callup-demo://abc=1">open in demo</a>
-app.on('open-url', function(event, data) {
-  event.preventDefault()
-  console.log('open by web:', data)
-})
+app.on('open-url', function (event, data) {
+  event.preventDefault();
+  console.log('open by web:', data);
+});
 // app.removeAsDefaultProtocolClient('callup-demo')
 // app.setAsDefaultProtocolClient('callup-demo')
 
